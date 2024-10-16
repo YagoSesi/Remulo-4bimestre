329d4efe1785a9a98bf6ef12278e381292669c15
Yago Maia <yago1452700@sesigoias.com.br>
2024-10-16 10:00:18 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import CadastroUsuario from './CadastroUsuario'
+import axios from 'axios'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <CadastroUsuario/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
