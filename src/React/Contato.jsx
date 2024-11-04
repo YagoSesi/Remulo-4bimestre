acfbcf666c746f56c7e822e02afb4d34ce5a3659
YagoSesi <yago1452700@sesigoias.com.br>
2024-11-04 12:05:22 -0300

    Porjeto

---
+++ src/React/Contato.jsx
@@ -0,0 +1,12 @@
+import Contato from "react";
+
+function Contato() {
+    return (
+        <section id="contato">
+            <h2>Contato</h2>
+            <p>LinkedIn | GitHub | Twitter</p>
+        </section>
+    );
+}
+
+export default Contato;
